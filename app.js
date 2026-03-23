ac3c597b2c385055c295b8f6d70f300165302ab6
Your Name <theo.melaert28@gmail.com>
2026-03-23 14:27:57 +0100

    preparing

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const app = express()
 
+
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+const server = app.listen(port,()=>{
+    console.log('le serveur marche ')
+})
